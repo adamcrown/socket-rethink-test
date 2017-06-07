bf00a406d4f9c4738e693577ab19a76edccad9cf
Adam Crownoble <adam@codenoble.com>
2017-06-07 11:28:54 +0800

    Add README and clarify some settings

---
+++ README.md
@@ -0,0 +1,27 @@
+Socket Rethink Test
+-------------------
+
+Simple proof of concept Node.js server using Socket.io as a layer on top of RethinkDB for real-time data pushing. It's meant to run alongside [rethink-socket-test](https://github.com/adamcrown/rethink-socket-test).
+
+Requirements
+------------
+
+- Node.js
+- NPM
+- RethinkDB
+
+Installing
+----------
+
+```shell
+git clone git@github.com:adamcrown/rethink-socket-test.git
+cd rethink-socket-test
+npm install
+```
+
+Running
+-------
+
+```javascript
+node server.js
+```
--- server.js
+++ server.js
@@ -2,9 +2,12 @@ var r = require('rethinkdb');
 var app = require('http').createServer();
 var io = require('socket.io')(app);
 
-app.listen(3001);
+var port = 3001;
+var db_name = 'rethink_socket_test_development';
 
-r.connect({db: 'rethink_socket_test_development'}).then(function(c) {
+app.listen(port);
+
+r.connect({db: db_name}).then(function(c) {
   r.table('messages').changes().run(c)
     .then(function(cursor) {
       cursor.each(function(err, item) {
@@ -13,4 +16,4 @@ r.connect({db: 'rethink_socket_test_development'}).then(function(c) {
     });
 });
 
-console.log('Ready...')
+console.log('Listening on port ' + port + '...')
